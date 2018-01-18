a78e634d70bb96e1c8fce4240c7d98163aec8d3e
Mac Mini <>
2018-01-18 14:47:00 +0530

    changes done until 18-jan-2018

--- functions.php
+++ functions.php
@@ -12,56 +12,43 @@
 <?php
 
   if (!is_admin())
-  {
-      add_action( 'wp_enqueue_scripts', 'truckindia_styles' );
-      add_action( 'wp_enqueue_scripts', 'truckindia_scripts' );
-  }
+    {
+        add_action( 'wp_enqueue_scripts', 'truckindia_styles' );
+        add_action( 'wp_enqueue_scripts', 'truckindia_scripts' );
+    }
 
   function truckindia_styles()
-  {
-    wp_enqueue_style("bootstrap", get_template_directory_uri(). "/stylesheets/bootstrap.min.css");
-    wp_enqueue_style("main-css", get_template_directory_uri(). "/stylesheets/main.css");
-    wp_enqueue_style("truckindia-style", get_template_directory_uri(). "/style.css");
-  }
+    {
+      wp_enqueue_style("bootstrap", get_template_directory_uri(). "/stylesheets/bootstrap.min.css");
+      wp_enqueue_style("ion-icons-custom", get_template_directory_uri(). "/ionicons/css/ionicons.css");
+      wp_enqueue_style("main-css", get_template_directory_uri(). "/stylesheets/main.css");
+      wp_enqueue_style("truckindia-style", get_template_directory_uri(). "/style.css");
+    }
 
   function truckindia_scripts()
-  {
-    wp_enqueue_script("main", get_template_directory_uri(). "/javascripts/libs/bootstrap.min.js",array(),false,true);
-
-  }
-
+    {
+      wp_enqueue_script("main", get_template_directory_uri(). "/javascripts/libs/bootstrap.min.js",array(),false,true);
+    }
 
   function truckindia_setup()
-  {
-    register_nav_menu('header', esc_html__( 'Header Navigation', 'truckindia'));
-    register_nav_menu('footer', esc_html__( 'Footer Navigation', 'truckindia'));
-
-    add_theme_support('post-thumbnails' );
-
-    add_theme_support( "title-tag" );
-
-    add_theme_support( 'custom-header' );
-
-    add_theme_support( "custom-background");
-
-
-    //Feed links
-    add_theme_support( 'automatic-feed-links' );
-
-    //Content width
-    if ( ! isset( $content_width ) ) $content_width = 900;
+    {
+      register_nav_menu('header', esc_html__( 'Header Navigation', 'truckindia'));
+      register_nav_menu('footer', esc_html__( 'Footer Navigation', 'truckindia'));
+      add_theme_support('post-thumbnails' );
+      add_theme_support( "title-tag" );
+      add_theme_support( 'custom-header' );
+      add_theme_support( "custom-background");
+      //Feed links
+      add_theme_support( 'automatic-feed-links' );
+      //Content width
+      if ( ! isset( $content_width ) ) $content_width = 900;
+      //Load the text domain
+      load_theme_textdomain('truckindia', get_template_directory() . '/languages');
+    }add_action( 'after_setup_theme', 'truckindia_setup' );
 
 
-    //Load the text domain
-    load_theme_textdomain('truckindia', get_template_directory() . '/languages');
-  }
+  function truckindia_fonts_url()
+    {
+      wp_enqueue_style( 'truckindia-fonts', 'https://fonts.googleapis.com/css?family=Hind:300,400,500,700|Montserrat:300,400,500,700|Open+Sans:300,400,700|Roboto+Condensed:300,400,700', false );
 
-  add_action( 'after_setup_theme', 'truckindia_setup' );
-
-
-  function truckindia_fonts_url() {
-
-  wp_enqueue_style( 'truckindia-fonts', 'https://fonts.googleapis.com/css?family=Hind:300,400,500,700|Montserrat:300,400,500,700|Open+Sans:300,400,700|Roboto+Condensed:300,400,700', false );
-
-  }
-  add_action( 'wp_enqueue_scripts', 'truckindia_fonts_url' );
+    }add_action( 'wp_enqueue_scripts', 'truckindia_fonts_url' );
--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<title>testing</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php bloginfo('name'); ?> <?php wp_title("|",true); ?>">
 <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
@@ -15,8 +14,79 @@
 </head>
 <body <?php body_class(); ?>>
 
+  <section class="white-2-bg social-block">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-6">
+          <div class="row justify-content-md-end">
+            <div class="col-7 col-md-auto">
+              <span class="font-size-1 gray-b font-h font-weight-700">ASK EXPERT</span> <div class="font-size-1 bray-l disin font-h font-weight-500"><a href="tel:+919846098460">+919846098460</a> | <a href="mailto:info@truckingindia.in">info@truckingindia.in</a></div>
+            </div>
+            <div class="col-2 col-md-auto">
+              <div class="social-block-icon">
+                <ul>
+                  <li>
+                    <a href="#" target="_blank"><i class="ion-social-instagram"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank"><i class="ion-social-facebook"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank"><i class="ion-social-twitter"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-3 col-md-auto">
+              <span class="font-size-1 gray-l font-h font-weight-500">ENG | HIN | TML</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-
+  <header class="nav">
+    <div class="container">
+      <div class="row mar-top-min mar-bot-min">
+        <div class="col-md-2">
+          <a href="<?php echo esc_url(home_url( '/' )); ?>">
+            <img src="wp-content/uploads/2018/01/logo.png" width="125px;">
+          </a>
+        </div>
+        <div class="col-md-7">
+          <div class="mar-top-min mar-bot-min">
+            <?php
+              $nav_args = array(
+                'theme_location'  => 'header',
+                'container'       => true,
+                'menu_class'      => '',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu'
+              );
+              wp_nav_menu( $nav_args );
+            ?>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="row no-gutters mar-top-min mar-bot-min compare-block">
+              <div class="col">
+                <a class="font-r font-size-2 font-weight-700" href="#">COMPARE</a>
+              </div>
+              <div class="col">
+                <div class="truck-compare-icon">
+                  <img class="compare-icon-img" src="wp-content/themes/truck_theme/img/truck.svg" alt="" width="45px" height="45px">
+                </div>
+              </div>
+              <div class="col">
+                <a class="font-r font-size-2 font-weight-700" href="#">BUY NOW</a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 
 
 
